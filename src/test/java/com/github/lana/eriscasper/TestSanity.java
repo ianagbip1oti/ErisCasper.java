28ee75e1bffe3d07bf0ecd8412f14bad9596136e
ianagbip1oti <ianagbip1oti@gmail.com>
2018-03-02 01:31:36 +0000

    Add JUnit5

---
+++ src/test/java/com/github/lana/eriscasper/TestSanity.java
@@ -0,0 +1,12 @@
+package com.github.lana.eriscasper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestSanity {
+
+  @Test
+  public void shouldBeSane() {
+    Assertions.assertTrue(true);
+  }
+}
