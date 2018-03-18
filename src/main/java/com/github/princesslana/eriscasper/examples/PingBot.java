925122611a5acefda9920ad9eba45d0c4bdd4f82
casper-h <caspertt2009@gmail.com>
2018-03-17 20:52:55 -0700

    Revert Changes

--- src/main/java/com/github/princesslana/eriscasper/examples/PingBot.java
+++ src/main/java/com/github/princesslana/eriscasper/examples/PingBot.java
@@ -5,41 +5,29 @@ import com.github.princesslana.eriscasper.event.Event;
 import com.github.princesslana.eriscasper.event.Events;
 import com.github.princesslana.eriscasper.rest.ImmutableSendMessageRequest;
 import com.github.princesslana.eriscasper.rest.RouteCatalog;
-import io.reactivex.Completable;
 
 public class PingBot {
 
   public static void main(String[] args) {
     ErisCasper.create()
         .run(
-            ctx -> {
-              Completable ping =
-                  ctx.getEvents()
-                      .ofType(Events.MessageCreate.class)
-                      .map(Event::getData)
-                      .filter(d -> d.getContent().equals("+ping"))
-                      .map(d -> RouteCatalog.createMessage(d.getChannelId()))
-                      .flatMapCompletable(
-                          r ->
-                              ctx.execute(
-                                      r,
-                                      ImmutableSendMessageRequest.builder().content("pong").build())
-                                      .toCompletable());
+            ctx ->
+                ctx.getEvents()
 
-              Completable testPing =
-                  ctx.getEvents()
-                      .ofType(Events.MessageCreate.class)
-                      .map(Event::getData)
-                      .filter(b -> b.getContent().equals("+test"))
-                      .map(b -> RouteCatalog.createMessage(b.getChannelId()))
-                      .flatMapCompletable(
-                          x ->
-                              ctx.execute(
-                                      x,
-                                      ImmutableSendMessageRequest.builder().content("testPhrase").build())
-                                      .toCompletable());
+                    // only message create events
+                    .ofType(Events.MessageCreate.class)
+                    .map(Event::getData)
 
-              return Completable.mergeArray(ping, testPing);
-            });
+                    // only with content "+ping"
+                    .filter(d -> d.getContent().equals("+ping"))
+
+                    // send create message request
+                    .map(d -> RouteCatalog.createMessage(d.getChannelId()))
+                    .flatMapCompletable(
+                        r ->
+                            ctx.execute(
+                                    r,
+                                    ImmutableSendMessageRequest.builder().content("pong").build())
+                                .toCompletable()));
   }
 }
